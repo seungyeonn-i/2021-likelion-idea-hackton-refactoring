858d3f2ddbe96e665aaca76c09fb82f6c14393da
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2021-08-07 15:27:47 +0900

    first commit

---
+++ src/main/java/portpofol/portpofolspring/PortpofolSpringApplication.java
@@ -0,0 +1,13 @@
+package portpofol.portpofolspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortpofolSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortpofolSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portpofol/portpofolspring/PortpofolSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package portpofol.portpofolspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortpofolSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
