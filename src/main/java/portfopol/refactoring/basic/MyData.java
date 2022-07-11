c492efaf307b228cc34a76e3da60786bc3580a03
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-07-12 03:11:00 +0900

    [#4]🔄 h2 database 연결

---
+++ src/main/java/portfopol/refactoring/basic/MyData.java
@@ -0,0 +1,12 @@
+package portfopol.refactoring.basic;
+
+import lombok.Data;
+
+@Data
+public class MyData {
+    private Long userId;
+    private String data;
+    private int year;
+    private int month;
+    private int day;
+}
---
+++ src/main/java/portfopol/refactoring/basic/MyUser.java
@@ -0,0 +1,12 @@
+package portfopol.refactoring.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class MyUser {
+
+    private Long userId;
+    private String userName;
+
+}
