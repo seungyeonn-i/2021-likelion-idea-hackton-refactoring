9ff8bfcac02d0846d1bf76b3d055bcd41b987b90
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-07-14 15:01:57 +0900

    ☑️ 요청 파라미터 조회 구현

--- src/main/java/portfopol/refactoring/basic/request/inputController.java
+++ src/main/java/portfopol/refactoring/basic/request/inputController.java
@@ -10,6 +10,45 @@ import java.util.Map;
 @Controller
 public class inputController {
 
+    @ResponseBody
+    @RequestMapping("/first-page")
+    public String inputPage1(@RequestParam("year") String year,
+                           @RequestParam("month") String month,
+                           @RequestParam("day") String day) {
+        log.info("year = {}, month = {}, day = {}", year, month, day);
+        //db 저장
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/second-page")
+    public String inputPage2(@RequestParam("content") String content) {
+        log.info("content = {}", content);
+        //db 저장
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/third-page")
+    public String inputPage3(@RequestParam("summary") String summary) {
+        log.info("summary = {}", summary);
+        //db 저장
+        return "ok";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/fourth-page") //data get 해오기
+    public String inputPage4(@RequestParam("summary") String summary) {
+        log.info("summary = {}", summary);
+        //db 저장
+        return "ok";
+    }
+
+
+
+
+
 
 
 }
--- src/main/java/portfopol/refactoring/basic/requestmapping/MappingClassController.java
+++ src/main/java/portfopol/refactoring/basic/requestmapping/MappingClassController.java
@@ -1,8 +1,9 @@
 package portfopol.refactoring.basic.requestmapping;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("mapping/")
 public class MappingClassController {
 
